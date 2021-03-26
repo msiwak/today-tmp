2454f502621a51ca34e20334310155d1b41546af
Marcin Siwak <marcin.siwak@gmail.com>
2021-03-26 22:06:58 +0100

    [#1] add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use clap::{crate_version, load_yaml, App};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("../args.yaml");
+    let _app = App::from_yaml(yaml).version(crate_version!()).get_matches();
 }
