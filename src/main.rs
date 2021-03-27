5283c916beb6e6ef8089f7585efae28c873bf5bc
Marcin Siwak <marcin.siwak@gmail.com>
2021-03-27 11:03:23 +0100

    [closes #5]: check if directory exists

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use clap::{crate_version, load_yaml, App};
 
 fn main() {
     let yaml = load_yaml!("../args.yaml");
-    let _app = App::from_yaml(yaml).version(crate_version!()).get_matches();
+    let _ = App::from_yaml(yaml).version(crate_version!()).get_matches();
 }
